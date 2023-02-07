677cf000e648181481570bebbc89a2bb4bc8ec56
Frank den Uijl <frankdenuijl@gmail.com>
2023-02-07 11:33:38 +0100

    Move GamificationPlayerManagerForWebGL logic to the base class

--- Packages/com.4growth.gamification-player-app-for-unity/Scripts/Runtime/GamificationPlayerManager.cs
+++ Packages/com.4growth.gamification-player-app-for-unity/Scripts/Runtime/GamificationPlayerManager.cs
@@ -257,6 +257,34 @@ namespace GamificationPlayer
             }
         }
 
+#if UNITY_WEBGL
+        public void Start()
+        {
+            if(!string.IsNullOrEmpty(Application.absoluteURL))
+            {
+                Uri url = new Uri(Application.absoluteURL);
+                var query = System.Web.HttpUtility.ParseQueryString(url.Query);
+                string jwt = query["moduleData"];
+
+                var json = JWTHelper.GetJSONWebTokenPayload(jwt, GamificationPlayerConfig.EnviromentConfig.JSONWebTokenSecret);
+
+                var dto = json.FromJson<MicroGamePayload>();
+
+                sessionData.AddToLog(dto);
+
+                if(sessionData.TryGetLatestModuleId(out Guid id))
+                {
+                    InvokeModuleStart(id);
+                }
+
+                if(sessionData.TryGetLatestMicroGamePayload(out MicroGamePayload microGamePayload))
+                {
+                    InvokeMicroGameOpened(microGamePayload);
+                }
+            }
+        }
+#endif
+
         private bool GTryGetServerTime(out DateTime dateTime)
         {
             dateTime = DateTime.Now;

9b2cab3c0ff60b99107a262e070b46b46f25599c
Frank den Uijl <frankdenuijl@gmail.com>
2023-02-16 11:14:49 +0100

    Able to test code without a JWT secret

--- Packages/com.4growth.gamification-player-app-for-unity/Scripts/Runtime/Helpers/JWTHelper.cs
+++ Packages/com.4growth.gamification-player-app-for-unity/Scripts/Runtime/Helpers/JWTHelper.cs
@@ -1,6 +1,7 @@
 using JWT;
 using JWT.Algorithms;
 using JWT.Serializers;
+using UnityEngine;
 
 namespace GamificationPlayer
 {
@@ -8,15 +9,22 @@ namespace GamificationPlayer
     {
         public static string GetJSONWebTokenPayload(string token, string secret)
         {
+            string json;
             var serializer = new JsonNetSerializer();
             var provider = new UtcDateTimeProvider();
             var validator = new JwtValidator(serializer, provider);
             var urlEncoder = new JwtBase64UrlEncoder();
             var algorithm = new HMACSHA256Algorithm();
             var decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
-            
-            var json = decoder.Decode(token, secret, true);
 
+            if(string.IsNullOrEmpty(secret))
+            {
+                Debug.LogWarning("JWT Secret is not set! This is a security vulnerability, please set it before going live!");
+                json = decoder.Decode(token, false);
+                return json;
+            }
+            
+            json = decoder.Decode(token, secret, true);
             return json;
         }
     }
